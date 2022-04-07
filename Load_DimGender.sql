459ceaa1c2934f9253be66389b44f7a8c4e735d0
mannasebastian <90005566+mannasebastian@users.noreply.github.com>
2022-04-07 17:30:41 -0400

    Add files via upload

--- Load_DimGender.sql
+++ Load_DimGender.sql
@@ -1,13 +1,22 @@
+USE [BIClass]
+GO
+/****** Object:  StoredProcedure [Project2].[Load_DimMaritalStatus]  ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
 -- =============================================
 -- Author: Manna Sebastian
 -- Create date: April 4, 2022
 -- Description:	Add the necessary information to the DimGender table
 -- Passed unit testing 04-06. Removed unneeded sequence
+-- Added WorkFlowStepTableRowCount column & UserAuthorizationKey column
 -- =============================================
-go
-drop procedure if exists [Project2].[Load_DimGender]
-go
-create PROCEDURE [Project2].[Load_DimGender] @UserAuthorizationKey INT
+GO
+DROP PROCEDURE IF EXISTS [Project2].[Load_DimGender]
+GO
+CREATE PROCEDURE [Project2].[Load_DimGender] @UserAuthorizationKey INT
 AS
 BEGIN
 
@@ -17,7 +26,7 @@ BEGIN
 
     SELECT @start = SYSDATETIME();
 
-	INSERT INTO [CH01-01-Dimension].DimGender (Gender, GenderDescription)
+	INSERT INTO [CH01-01-Dimension].DimGender (Gender, GenderDescription, UserAuthorizationKey)
 
     SELECT Gender,
     GenderDescription = CASE
@@ -25,18 +34,22 @@ BEGIN
         'Female'
     ELSE 
         'Male'
-    END
+    END, @UserAuthorizationKey
+    
     FROM
     (SELECT DISTINCT Gender FROM FileUpload.OriginallyLoadedData)
     As G
 
-
 	SELECT @end = SYSDATETIME();
 
+    DECLARE @rowcount AS INT 
+    SET @rowcount = (SELECT count(*)
+    FROM [CH01-01-Dimension].[DimGender]);
 
     EXEC [Process].[usp_TrackWorkFlow]
      @StartTime = @start,
      @EndTime = @end,
-     @WorkFlowDescription = 'Load DimGender',
+     @WorkFlowDescription = 'Load DimGender Table',
+     @WorkFlowStepTableRowCount = @rowcount,
      @UserAuthorizationKey = @UserAuthorizationKey
 END
--- Load_DimMaritalStatus.sql
+++ Load_DimMaritalStatus.sql
@@ -3,12 +3,13 @@
 -- Create date: April 4, 2022
 -- Description:	Add the necessary information to the DimMaritalStatus table
 -- Passed unit testing 04-06. Took out sequence (not needed) 
+-- Added WorkFlowStepTableRowCount column & UserAuthorizationKey column
 -- =============================================
-use BIClass
+USE BIClass
 
-drop procedure if exists [Project2].[Load_DimMaritalStatus]
-go
-create PROCEDURE [Project2].[Load_DimMaritalStatus] @UserAuthorizationKey INT
+DROP PROCEDURE IF EXISTS [Project2].[Load_DimMaritalStatus]
+GO
+CREATE PROCEDURE [Project2].[Load_DimMaritalStatus] @UserAuthorizationKey INT
 AS
 BEGIN
 
@@ -18,7 +19,7 @@ BEGIN
 
     SELECT @start = SYSDATETIME();
 
-	INSERT INTO [CH01-01-Dimension].DimMaritalStatus (MaritalStatus, MaritalStatusDescription)
+	INSERT INTO [CH01-01-Dimension].DimMaritalStatus (MaritalStatus, MaritalStatusDescription, UserAuthorizationKey)
 
     SELECT MaritalStatus,
     MaritalStatusDescription = CASE
@@ -26,18 +27,23 @@ BEGIN
             'Married'
         ELSE 
             'Single'
-    END
-    from 
+    END, @UserAuthorizationKey
+
+    FROM 
     (SELECT DISTINCT MaritalStatus FROM FileUpload.OriginallyLoadedData)
     AS A
 
 	SELECT @end = SYSDATETIME();
 
+    DECLARE @rowcount AS INT 
+    SET @rowcount = (SELECT count(*)
+    FROM [CH01-01-Dimension].[DimMaritalStatus]);
 
     EXEC [Process].[usp_TrackWorkFlow]
      @StartTime = @start,
      @EndTime = @end,
      @WorkFlowDescription = 'Load DimMaritalStatus',
+     @WorkFlowStepTableRowCount = @rowcount,
      @UserAuthorizationKey = @UserAuthorizationKey
 
 END
