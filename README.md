ff7b03991b4e67778e49b6b42720d547071b24a2
N Walton <nick.walton77@qmail.cuny.edu>
2022-04-15 19:45:27 -0400

    Fixing bad commit & updating README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Team 10_C Project 2
+## CS331 Spring 22
+
+### Recreate the BIClass database
+
+- This is a collection of stored procedures that drop and recreate a star schema with the proper constraints and relationships within an existing database.
+
+- Note that MAIN.sql contains all project code, but it is not sufficient to run MAIN.sql to recreate the star schema.
+
+- There are conditions that must be met before many of the procedures can be executed (certain columns need be added, tables created, etc)
+
+- TODO: add STEP1, STEP2, STEP3 .sql files that contain the code properly organized to achieve the end result.
--- ShowWorkFlowSteps.sql
+++ ShowWorkFlowSteps.sql
@@ -1,4 +1,24 @@
-CREATE PROCEDURE [Process].[usp_ShowTrackWorkFlow]
+drop procedure if exists [Process].[usp_ShowWorkflowSteps];
+go
+
+CREATE PROCEDURE [Process].[usp_ShowWorkflowSteps]
 AS
- SELECT * FROM process.workflowsteps
+
+    SELECT * FROM process.workflowsteps;
+
+    select DATEDIFF(second, min(StartingDateTime), max(EndingDateTime)) as TimeElapsedSeconds,  DATEDIFF(second, min(StartingDateTime), max(EndingDateTime)) / 60 as TimeElapsedMinutes
+	from Process.WorkflowSteps
+
+    select  db.GroupMemberFirstName, 
+    db.UserAuthorizationKey, 
+    count(WorkFlowStepKey) as NumProcedures, 
+    sum(datediff(millisecond, StartingDateTime, EndingDateTime)) / 1000 as executionTimeInSeconds, 
+    sum(datediff(MILLISECOND, StartingDateTime, EndingDateTime)) as executionTimeInMS
+    from process.WorkflowSteps as wfs
+    left join DbSecurity.UserAuthorization as db
+    on db.UserAuthorizationKey = wfs.UserAuthorizationKey
+    group by db.UserAuthorizationKey, db.GroupMemberFirstName
+
 GO
+
+end
